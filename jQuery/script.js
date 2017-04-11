4019f6afc535390a5dc395bde5cf41cde1a4ed68
Hstamp <hattie.stamp@gmail.com>
2017-04-11 11:02:19 +0100

    Completing second page code, adding third page
    - Completed second page Javascript
    - Added 3rd page + Javascript
    - Imported Bootstrap (into index page only)

--- jQuery/script.js
+++ jQuery/script.js
@@ -13,11 +13,15 @@ $(document).ready(
   function(){
     $("#show_pip_rates").hide();
     $("#show_dla_rates").hide();
+    $("#show_weekly_rates").hide();
 
     $("#show_pip").click(function () {
       $("#show_pip_rates").toggle();
     });
     $("#show_dla").click(function () {
       $("#show_dla_rates").toggle();
+    });
+    $("#show_allowance").click(function () {
+      $("#show_weekly_rates").toggle();
     });
 });
